59dda0a88d48423304816922612026be5cf556ca
MasterMobo <mastermobo101@gmail.com>
2023-04-16 01:59:13 +0700

    update page

--- automation/detail-page-data.js
+++ automation/detail-page-data.js
@@ -39,12 +39,56 @@ const data = [
         ],
         cover_img: "../../images/books/egghead.jpg",
         sub_imgs: [
-            "../../images/books/dune-sub1.png",
-            "../../images/books/dune-sub2.png",
-            "../../images/books/dune-sub3.png",
+            "../../images/books/egghead-sub1.jpg",
+            "../../images/books/egghead-sub2.jpg",
+            "../../images/books/egghead-sub3.jpg",
         ],
         price: 23.61,
         breadcrumb_name: ["Home", "Best Sellers", "Egghead"],
+        breadcrumb_link: ["../../index.html", "../best-sellers.html", "#!"],
+    },
+
+    {
+        file_name: "american_mermaid",
+        title: "American Mermaid",
+        author: "Julia Langbein",
+        desc: [
+            `"Brilliantly sharp, funny, and thought-provoking, the gripping story of a woman trying to find her way in our chaotic world." —Madeline Miller, bestselling author of Circe`,
+            `Broke English teacher Penelope Schleeman is as surprised as anyone when her feminist novel American Mermaid becomes a best-seller.  Lured by the promise of a big payday, she quits teaching and moves to L.A. to turn the novel into an action flick with the help of some studio hacks. But as she's pressured to change her main character from a fierce, androgynous eco-warrior to a teen sex object in a clamshell bra, strange things start to happen. Threats appear in the screenplay; siren calls lure Penelope’s co-writers into danger.  Is Penelope losing her mind, or has her mermaid come to life, enacting revenge for Hollywood’s violations?
+            `,
+            `American Mermaid follows a young woman braving the casual slights and cruel calculations of a ruthless industry town, where she discovers a beating heart in her own fiction, a mermaid who will fight to move between worlds without giving up her voice. A hilarious story about deep things, American Mermaid asks how far we’ll go to protect the parts of ourselves that are not for sale.
+            `,
+        ],
+        cover_img: "../../images/books/american_mermaid.jpg",
+        sub_imgs: [
+            "../../images/books/american_mermaid-sub1.jpg",
+            "../../images/books/american_mermaid-sub2.jpg",
+            "../../images/books/american_mermaid-sub3.jpg",
+        ],
+        price: 14.99,
+        breadcrumb_name: ["Home", "Best Sellers", "American Mermaid"],
+        breadcrumb_link: ["../../index.html", "../best-sellers.html", "#!"],
+    },
+
+    {
+        file_name: "above_ground",
+        title: "Above Ground",
+        author: "Clint Smith",
+        desc: [
+            `A remarkable poetry collection with "inextinguishable generosity and abundant wisdom" (Monica Youn) from Clint Smith, the #1 New York Times bestselling and National Book Critics Circle award-winning author of How the Word Is Passed.`,
+            `Clint Smith’s vibrant and compelling new collection traverses the vast emotional terrain of fatherhood, and explores how becoming a parent has recalibrated his sense of the world. There are poems that interrogate the ways our lives are shaped by both personal lineages and historical institutions. There are poems that revel in the wonder of discovering the world anew through the eyes of your children, as they discover it for the first time. There are poems that meditate on what it means to raise a family in a world filled with constant social and political tumult. Above Ground wrestles with how we hold wonder and despair in the same hands, how we carry intimate moments of joy and a collective sense of mourning in the same body. Smith’s lyrical, narrative poems bring the reader on a journey not only through the early years of his children’s lives, but through the changing world in which they are growing up—through the changing world of which we are all a part.
+            `,
+            `Above Ground is a breathtaking collection that follows Smith's first award-winning book of poetry, Counting Descent.
+            `,
+        ],
+        cover_img: "../../images/books/above_ground.jpg",
+        sub_imgs: [
+            "../../images/books/above_ground-sub1.jpg",
+            "../../images/books/above_ground-sub2.jpg",
+            "../../images/books/above_ground-sub3.jpg",
+        ],
+        price: 12.99,
+        breadcrumb_name: ["Home", "Best Sellers", "Above Ground"],
         breadcrumb_link: ["../../index.html", "../best-sellers.html", "#!"],
     },
 ];
